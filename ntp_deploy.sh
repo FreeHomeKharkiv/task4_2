6088fb45e4d323c9b933ecefb308399eae5fe4f0
AntonLev89 <anton2014lev@gmail.com>
2018-04-01 14:56:48 +0300

    ntp_deploy.sh

--- ntp_deploy.sh
+++ ntp_deploy.sh
@@ -12,12 +12,13 @@ sed -i 's/pool [0-9].ubuntu.pool.ntp.org iburst/pool 0.ua.pool.ntp.org iburst/' 
 sed -i 's/pool ntp.ubuntu.com/pool ua.pool.ntp.org/' /etc/ntp.conf
 ###uniq -u /etc/ntp.conf > /etc/ntp.conf
 
+###Restart your NTP Server
+/etc/init.d/ntp restart
+
 touch /etc/cron.d/ntpserver
 chmod 777 /etc/cron.d/ntpserver
-echo "[0-59] * * * * /etc/MirantisT/task_2/ntp_deploy.sh" >> /etc/cron.d/ntpserver
+echo "[0-59] * * * * /etc/MirantisT/task_2/ntp_verify.sh" >> /etc/cron.d/ntpserver
 
-###Start NTP Server
-/etc/init.d/ntp restart
 # 
 
 ###
